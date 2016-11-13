58dcbca27b60037006c572f79d754bc2515735b6
Lin, Tzu-Heng <lzhbrian@gmail.com>
2016-11-13 17:31:17 +0800

    Writing RBM

--- metropolis_hastings/metropolis_hasting.R
+++ metropolis_hastings/metropolis_hasting.R
@@ -25,7 +25,7 @@ norm.probability <- function(x, mu, sigma){
 test.sd.T <- function(sd.T, plot.or.not) {
   
   # Setup some arg
-  # set.seed(50)
+  set.seed(50)
   mu = c(5,10)
   sigma = matrix(c(1,1,1,4), nrow=2, ncol=2, byrow = TRUE)
   real_corr = sqrt( (sigma[1,2]*sigma[2,1])/(sigma[1,1]*sigma[2,2]) ) # Theoretical corelation
@@ -45,7 +45,9 @@ test.sd.T <- function(sd.T, plot.or.not) {
     # Generate a new x with norm(mean=cur.x, sd=given.by.me)
     new.f <- norm.probability(new.x, mu, sigma) 
     if ( runif(1) < min(1,new.f/cur.f) ) { # transfer to another value
-      accept.num <- accept.num + 1
+      if (i > sample.num/2) {
+        accept.num <- accept.num + 1
+      }
       cur.x <- new.x
       cur.f <- new.f
     } else { # retain the original value
@@ -55,8 +57,10 @@ test.sd.T <- function(sd.T, plot.or.not) {
     random.matrix[i,] <- cur.x
   }
   
-  accepting.rate <- accept.num/sample.num
-  pearson.correlation <- cor.test(random.matrix[,1], random.matrix[,2])$estimate
+  accepting.rate <- accept.num/sample.num*2
+  pearson.correlation <- cor.test(
+    random.matrix[(sample.num/2):sample.num,1], 
+    random.matrix[(sample.num/2):sample.num,2])$estimate
   
   # plot, output results
   if (plot.or.not==1) {
@@ -66,7 +70,7 @@ test.sd.T <- function(sd.T, plot.or.not) {
     cat( paste("Theoretically correlation:", real_corr, '\n' ) )
     cat( paste("Real correlation:", pearson.correlation, '\n') )
     # Plot
-    plot(random.matrix, 
+    plot(random.matrix[(sample.num/2):sample.num,], 
          xlab = expression(x["1"]), ylab = expression(x["2"]), 
          col = 'blue', cex.main = 0.75)
   }
@@ -75,33 +79,33 @@ test.sd.T <- function(sd.T, plot.or.not) {
   
 }
 
-# # Test best sd.T: jumping distance
-# test.range <- seq(1.5, 5, 0.1)
-# acc.rate.vec = vector(length = length(test.range))
-# cor.vec = vector(length = length(test.range))
-# for (i in c(1:length(test.range))) {
-#   ans_list <- test.sd.T(test.range[i],0)
-#   acc.rate.vec[i] <- ans_list[[1]]
-#   cor.vec[i] <- ans_list[[2]]
-#   cat(i, test.range[i], '\n')
-# }
-# 
-# # Plot Accepting Rate - sd.T
-# plot(test.range, acc.rate.vec, 
-#      type="l", # main = 'Accepting Rate - sd.T', 
-#      xlab = 'sd.T', ylab = 'Accepting Rate',
-#      cex.main = 0.75, cex.axis=1)
-# lines(test.range, rep(0.234,time=length(test.range)), col = "red")
-# text(4.2, 0.25, "Acc.rate = 0.234", cex = 0.8, col="red")
-# text(4.2, 0.22, "sd.T = 3.0", cex = 0.8, col="red")
+# Test best sd.T: jumping distance
+test.range <- seq(1.5, 5, 0.1)
+acc.rate.vec = vector(length = length(test.range))
+cor.vec = vector(length = length(test.range))
+for (i in c(1:length(test.range))) {
+  ans_list <- test.sd.T(test.range[i],0)
+  acc.rate.vec[i] <- ans_list[[1]]
+  cor.vec[i] <- ans_list[[2]]
+  cat(i, test.range[i], '\n')
+}
 
-# Use sd.T = 3.0
-set.seed(2333224)
-sd.T = 3.0
-ans_list <- test.sd.T(sd.T, 1)
-acc.rate <- ans_list[[1]]
-cor <- ans_list[[2]]
-cat(acc.rate, cor, '\n')
+# Plot Accepting Rate - sd.T
+plot(test.range, acc.rate.vec,
+     type="l", # main = 'Accepting Rate - sd.T',
+     xlab = 'sd.T', ylab = 'Accepting Rate',
+     cex.main = 0.75, cex.axis=1)
+lines(test.range, rep(0.234,time=length(test.range)), col = "red")
+text(4.2, 0.25, "Acc.rate = 0.234", cex = 0.8, col="red")
+text(4.2, 0.22, "sd.T = 3.0", cex = 0.8, col="red")
+
+# # Use sd.T = 3.0
+# set.seed(233312)
+# sd.T = 3.0
+# ans_list <- test.sd.T(sd.T, 1)
+# acc.rate <- ans_list[[1]]
+# cor <- ans_list[[2]]
+# cat(acc.rate, cor, '\n')
 
 
 # plot(test.range, cor.vec, 
